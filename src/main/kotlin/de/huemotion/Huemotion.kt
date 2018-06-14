641e9f1113bab97bae132a099b99028210b9b9ff
Julian Sauer <julian_sauer@gmx.net>
2018-06-14 02:51:38 +0200

    Add speech to text request

--- src/main/kotlin/de/huemotion/Huemotion.kt
+++ src/main/kotlin/de/huemotion/Huemotion.kt
@@ -1,11 +1,14 @@
 package de.huemotion
 
+import de.huemotion.sound.getText
 import khttp.structures.authorization.BasicAuthorization
 import java.net.URLEncoder
 
+
 fun main(args: Array<String>) {
     val config = ConfigManager()
-    val text = "Team, I know that times are tough! Product sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it!"
+    val text = getText(config.speechToTextUser, config.speechToTextPassword)
+    println("Text: $text")
     val url = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false&text=" + URLEncoder.encode(text, "UTF-8")
-    print(khttp.get(url = url, auth= BasicAuthorization(config.user, config.password)).jsonObject)
+    print(khttp.get(url = url, auth = BasicAuthorization(config.toneAnalyzerUser, config.toneAnalyzerPassword)).jsonObject)
 }
---
+++ src/main/kotlin/de/huemotion/sound/SpeechToText.kt
@@ -0,0 +1,43 @@
+package de.huemotion.sound
+
+import org.apache.commons.io.IOUtils
+import org.json.JSONObject
+import java.net.Authenticator
+import java.net.HttpURLConnection
+import java.net.PasswordAuthentication
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun getText(user: String, password: String): String {
+    val fileName = "Recording.wav"
+
+    val recorder = Recorder()
+    recorder.record(10, "Recording.wav")
+
+    Authenticator.setDefault(object : Authenticator() {
+        override fun getPasswordAuthentication(): PasswordAuthentication {
+            return PasswordAuthentication(user, password.toCharArray())
+        }
+    })
+
+    val url = URL("https://stream.watsonplatform.net/speech-to-text/api/v1/recognize")
+    val connection = url.openConnection() as HttpURLConnection
+    connection.doOutput = true
+    connection.requestMethod = "POST"
+    connection.setRequestProperty("Content-Type", "audio/wav")
+
+    val path = Paths.get(fileName)
+    connection.outputStream.write(Files.readAllBytes(path))
+    val result = IOUtils.toString(connection.inputStream)
+    return parseJSONResponse(result)
+}
+
+private fun parseJSONResponse(result: String): String {
+    var json = JSONObject(result)
+    var resultsArray = json.getJSONArray("results")
+    json = resultsArray.getJSONObject(0)
+    resultsArray = json.getJSONArray("alternatives")
+    json = resultsArray.getJSONObject(0)
+    return json.get("transcript").toString()
+}
